fd5509628d0d30c0cbe0522aaa801f4efdf707d4
Sander Laarhoven <lesander@xs4all.nl>
2014-03-22 16:26:02 +0100

    Trying to fix pastebin again.

--- pastebin-files/pastebin-install-client.lua
+++ pastebin-files/pastebin-install-client.lua
@@ -1,9 +1,12 @@
 -- GlassChat 3 Client Pastebin Updater/Installer
 -- By Tiiger87 and Alexandrov01
 getfile = http.get("https://raw.github.com/lesander/GlassChat3/master/pastebin-files/pastebin-install-client.lua")
+print("Got update file from github...")
 file = fs.open("temp-update", "w")
     file.write( getfile.readAll() )
 file.close()
+print("Created temp file...")
+print("Running temp file...")
 shell.run("temp-update")
 sleep(1)
 shell.run("rm", "temp-update")
