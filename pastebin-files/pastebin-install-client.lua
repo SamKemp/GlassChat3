07636da877f65d4c9ce36b76f85ff20cdc544e7c
Sander Laarhoven <lesander@xs4all.nl>
2014-03-22 16:21:22 +0100

    Put the pastebin on GitHub in case we ever lose it.

---
+++ pastebin-files/pastebin-install-client.lua
@@ -0,0 +1,8 @@
+-- GlassChat 3 Client Pastebin Updater/Installer
+-- By Tiiger87 and Alexandrov01
+int = http.get("https://raw.github.com/lesander/GlassChat3/master/pastebin-files/pastebin-install-client.lua")
+file = fs.open("temp-update", "w")
+    file.write( int.readAll() )
+file.close()
+shell.run("temp-update")
+shell.run("rm", "temp-update")
