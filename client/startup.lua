411b393a8e119d029757a06cb7de24c535572432
Toomas Tamm <tippa@hot.ee>
2014-03-22 17:39:05 +0200

    Attempt to fix modem and bridge detection

--- client/startup.lua
+++ client/startup.lua
@@ -21,10 +21,10 @@
     
     --Gets terminal bridge and wired modem side
     for k,v in pairs(redstone.getSides()) do
-        if peripheral.getType(v)==modem then
-            clientM = peripheral.wrap(v)
-        elseif peripheral.getType(v)==openperipheral_glassesbridge then
-            clientB = peripheral.wrap(v)
+        if peripheral.getType(k)==modem then
+            clientM = peripheral.wrap(k)
+        elseif peripheral.getType(k)==openperipheral_glassesbridge then
+            clientB = peripheral.wrap(k)
         end
     end
     
