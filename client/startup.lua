1efeead45311e1b42b807f8ed865c93b66a9e397
Toomas Tamm <tippa@hot.ee>
2014-03-22 16:58:22 +0200

    Advanced computer detection

--- client/startup.lua
+++ client/startup.lua
@@ -1,9 +1,15 @@
 -- GlassChat 3 Client By tiiger87 and alexandrov01
 
+--Checks if computer is an advnaced computer, if not then the script gets halted
+if term.isColor() == false then
+ print("Not an advanced computer! Please check your computer setup")
+ os.exit()
+end
+
 clientM = nil
 clientB = nil
 
---Gets terminal bridge and wired modem side
+--Gets terminal bridge and wired modem side, if one of them is not detected the script gets halted
 for k,v in pairs(redstone.getSides()) do
  if peripheral.getType(v)==modem then
   clientM = peripheral.wrap(v)
@@ -14,4 +20,5 @@ end
 
 if clientM == nil or clientB = nil then
  print("Missing modem or bridge! Please check your computer setup")
+ os.exit()
 end
