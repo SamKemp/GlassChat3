47505653809285f78fa806a89c26c2f513333403
Toomas Tamm <tippa@hot.ee>
2014-03-23 19:25:47 +0200

    testfix

--- client/startup.lua
+++ client/startup.lua
@@ -177,7 +177,7 @@ function receiveChat()
      refreshHUD()
      table.remove(scroll, 1)
       for key1, value1 in pairs(scroll) do
-        text = clientB.addText(x, y, string.sub(value1, 9), string.sub(value1, 1 , 8))
+        text = clientB.addText(x, y, string.sub(value1, 9), tonumber ( string.sub(value1, 1 , 8) ) )
         y = y + z
       end
     end
