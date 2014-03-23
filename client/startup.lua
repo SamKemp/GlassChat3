76386e588ebf0d100c2cd0c32b74bd8eeb2aacfc
Toomas Tamm <tippa@hot.ee>
2014-03-23 16:03:33 +0200

    testfix

--- client/startup.lua
+++ client/startup.lua
@@ -134,7 +134,7 @@ function sendChat()
            
           elseif string.match(msg_low, 'update$') then
            text = clientB.addText(x, y, "GlassChat ".. clientV .." - Updating your client", 0xFFFF00)
-           rednet.send(chatRoom, "!gc updating")
+           rednet.send(clientS, "!gc updating")
            shell.run("update")
           else
            text = clientB.addText(x, y, "Invalid command! Do $$help", 0xDAA520)
