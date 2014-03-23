a62b6cd46fa2aa9f1a93182f045c5e131a37cda2
Toomas Tamm <tippa@hot.ee>
2014-03-23 16:28:49 +0200

    debug

--- client/startup.lua
+++ client/startup.lua
@@ -157,7 +157,7 @@ function receiveChat()
    print(message)
    table.insert(scroll, message)
    if y >= startscroll then
-    print("autoscrolling!")
+    print(x.."autoscrolling!"..y)
     refreshHUD()
     table.remove(scroll, 1)
       for key1, value1 in pairs(scroll) do
@@ -165,7 +165,7 @@ function receiveChat()
         y = y + z
       end
     else
-     print("autoscroll is not neccecary yet.")
+     print(x.."autoscroll not neccecary"..y)
      text = clientB.addText(x, y, message, 0xFFFFFF)
      y = y + z
     end
