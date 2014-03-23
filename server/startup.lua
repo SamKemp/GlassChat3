51af07ba6ed1407033cba942459ff3f9840f7ce4
Sander Laarhoven <lesander@xs4all.nl>
2014-03-23 16:52:11 +0100

    Added server key presses commands and tidied up the file.

--- server/startup.lua
+++ server/startup.lua
@@ -26,21 +26,19 @@
         error()
     end
 
+    -- Variables
     serverV = "3.0.0 BETA";
     serverID = os.getComputerID()
     serverM = nil
 
     local names = {}
+    if fs.exists("data/names") then
+        file3 = fs.open("data/names","r")
+            snames = file3.readAll()
+            names = textutils.unserialize(snames)
+        file3.close()
+    end
 
-        if fs.exists("data/names") then
-         file3 = fs.open("data/names","r")
-          snames = file3.readAll()
-          names = textutils.unserialize(snames)
-         file3.close()
-        end
-
-    
-    
     -- Print basic info to screen
     term.clear()
     term.setCursorPos(1,1)
@@ -48,52 +46,78 @@
     print("GlassChat 3 Server "..serverV)
     term.setTextColor(colors.white)
     print("ID: "..serverID)
+    print("U: Update all clients.")
+    print("R: Restart all clients.")
+    print("S: Update server.")
     print("----------------------")
+   
    -- Functions
-
-function clientRequests()
- while true do
-   id, msg = rednet.receive()
-     
-    if string.len(msg) >= 1 then
-
-     if string.match(msg, '^!gc') then
-
-      if string.match(msg, '^!gc username') then
-	names[id] = string.sub(msg, 14)
-	rednet.broadcast("Computer "..id.." joined under the name of "..names[id]..".")
-        print(names[id].."("..id..") joined.")
-
-        file2 = fs.open("data/names","w")
-        file2.write( textutils.serialize( names ) )
-        file2.close()
-
-      elseif string.match(msg, '^!gc newusername') then
-        newname = string.sub(msg, 17)
-	rednet.broadcast(names[id].." has changed his/her name to "..newname..".")
-        print(names[id].." has changed his/her name to "..newname..".")
-        names[id] = newname
-
-        file2 = fs.open("data/names","w")
-        file2.write( textutils.serialize( names ) )
-        file2.close()
-
-      elseif string.match(msg, 'stopping$') then
-          print(names[id].."("..id..") has left. ")
-          rednet.broadcast(names[id]..": has left")
-
-      elseif string.match(msg, 'update') then
-          print(names[id].."("..id..") is updating. ")
-          rednet.broadcast(names[id]..": is updating.")
-      else
-       end
-
-     elseif string.len( names[id] ) >= 1 then
-          print(names[id].."("..id..") - "..msg)
-          rednet.broadcast(names[id]..": "..msg)
+    
+    -- Client Requests
+    function clientRequests()
+        while true do
+            id, msg = rednet.receive()
+            if string.len(msg) >= 1 then
+                if string.match(msg, '^!gc') then
+                    if string.match(msg, '^!gc username') then
+                        names[id] = string.sub(msg, 14)
+                        rednet.broadcast("Computer "..id.." joined under the name of "..names[id]..".")
+                        print(names[id].."("..id..") joined.")
+                        file2 = fs.open("data/names","w")
+                        file2.write( textutils.serialize( names ) )
+                        file2.close()
+                        
+                    elseif string.match(msg, '^!gc newusername') then
+                        newname = string.sub(msg, 17)
+                        rednet.broadcast(names[id].." has changed his/her name to "..newname..".")
+                        print(names[id].." has changed his/her name to "..newname..".")
+                        names[id] = newname
+                        file2 = fs.open("data/names","w")
+                        file2.write( textutils.serialize( names ) )
+                        file2.close()
+                        
+                    elseif string.match(msg, 'stopping$') then
+                        print(names[id].."("..id..") has left. ")
+                        rednet.broadcast(names[id]..": has left")
+                        
+                        
+                    elseif string.match(msg, 'update') then
+                        print(names[id].."("..id..") is updating. ")
+                        rednet.broadcast(names[id]..": is updating.")
+                    else
+                        
+                    end
+                elseif string.len( names[id] ) >= 1 then
+                    print(names[id].."("..id..") - "..msg)
+                    rednet.broadcast(names[id]..": "..msg)
+                end
+            end
+        end
     end
-   end
- end
-end
+    
+    -- Server key commands
+    function serverCommands()
+      while true do
+         local evt, c = os.pullEvent("char") -- wait for a key press
+         c = string.lower(c) -- convert to lower case, to register both r & R.
+            if c == "r" then
+                print("Server - R pressed. Restarting all clients.")
+                    rednet.broadcast("!gc all clients restart")
+		    sleep(0.1)  -- Delay to stop all computers from connecting at the same time
+                    
+            elseif c == "u" then
+                print("Server - U pressed. Updating all clients.")
+                    rednet.broadcast("!gc all clients update")
+		    sleep(0.1) -- Delay to stop all computers from connecting at the same time
+                    
+            elseif c == "s" then
+                print("Server - S pressed. Updating and Rebooting server.")
+                    rednet.broadcast("Server is updating and restarting.")
+                shell.run("update")
+            end
+	sleep(0)
+      end
+    end
 
-parallel.waitForAny(clientRequests)
+    -- Wait for any...
+    parallel.waitForAny(clientRequests, serverCommands)
