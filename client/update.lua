07efc0426f7ef374631048eeccf2878680ea9d17
Toomas Tamm <tippa@hot.ee>
2014-03-23 18:45:03 +0200

    x and y value set

--- client/update.lua
+++ client/update.lua
@@ -9,7 +9,7 @@
     end
 
 clientB.clear()
-text = clientB.addText(x, y, "GlassChat is updating!", 0xFFFF00)
+text = clientB.addText(20, 63, "GlassChat is updating!", 0xFFFF00)
 
 term.clear()
 term.setCursorPos(1,1)
